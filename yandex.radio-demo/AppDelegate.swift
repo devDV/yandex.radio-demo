d362a83140019028fa07fddcb9298aab602a5596
Denis Tkachenko <dattk.tk@gmail.com>
2019-08-03 12:40:32 +1000

    Initial Commit

---
+++ yandex.radio-demo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  yandex.radio-demo
+//
+//  Created by dattk on 03/08/2019.
+//  Copyright © 2019 dattk. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
