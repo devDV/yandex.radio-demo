fe045003862841e8d0a7a9eae8a53f10896b0f3b
Denis Tkachenko <dattk.tk@gmail.com>
2019-08-03 15:09:35 +1000

    flow протоколы и пара функций в appDelegate

--- yandex.radio-demo/AppDelegate.swift
+++ yandex.radio-demo/AppDelegate.swift
@@ -22,6 +22,23 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        NSLog(#function + flag.description)
+        sender.activate(ignoringOtherApps: true)
+        if !flag, let window = sender.windows.first {
+            window.makeKeyAndOrderFront(self)
+        }
+        return true
+    }
+    
+    func switchTo(view: NSView) {
+        guard let mainView = window.contentView else {return}
+        view.frame = mainView.bounds
+        view.autoresizesSubviews = true
+        view.autoresizingMask = [NSView.AutoresizingMask.width, NSView.AutoresizingMask.height]
+//        oldView?.removeFromSuperview()
+        mainView.addSubview(view)
+    }
 
 }
 
---
+++ yandex.radio-demo/api/flow.swift
@@ -0,0 +1,43 @@
+//
+//  flow.swift
+//  yandex.radio-demo
+//
+//  Created by dattk on 03/08/2019.
+//  Copyright © 2019 dattk. All rights reserved.
+//
+
+import Foundation
+
+protocol FlowControl {
+    func toggle()
+    func next()
+    func like()
+    func dislike()
+    func navigate(to: String)
+}
+
+enum FlowSource {
+    case radio
+    case music
+}
+
+protocol FlowSourceDelegate {
+    func onReady(controls: apiControlsAvailability)
+    func onChange(controls: apiControlsAvailability) // при изменнии доступности функций
+    func onChange(source: apiSource) // при изменении радиостанции
+    func onChange(state isPlaying: Bool) // при изменении состояния плейера
+    func onChange(track: apiTrack) // при изменении текущего трека
+    func onChange(playlist: apiPlaylist) // при изменении в наборе треков
+    //    func onChangeFeedback() // при лайках/дизлайках
+    //    func onChangeVolume() // при изменении уровня громкости
+    func onChange(advert: Bool, info: apiAdvert?)
+    func onChange(liked: Bool, info: apiTrack?)
+    func onChange(trackProgress: apiTrackProgress) // при изменении позиции в треке
+}
+
+protocol RadioSourceDelegate: FlowSourceDelegate {
+//    func onChange(station list: [apiStation])
+}
+
+protocol MusicSourceDelegate: FlowSourceDelegate {
+}
